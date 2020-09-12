3c03a96aa880d6837215560e73434cc9d864f902
samurailobster <jeffreymccharen@gmail.com>
2020-09-12 14:02:06 -0500

    Some refactoring...

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,19 @@ fn call_verse(each_lyric : u8) {
                  "Seven swans a swimming", "Eight maids a milking", "Nine ladies dancing", "Ten lords a leaping", "Eleven pipers piping", "Twelve drummers drumming"];
 
    match each_lyric {
+       // There's gotta be a better way.
         1 => println!("On the first day of Christmas {} {}", WHO, gifts[0]),
         2 => println!("On the second day of Christmas {} {} {}", WHO, gifts[1], gifts[0]),
-        3 => println!("On the third day of Christmas {} Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        4 => println!("On the fourth day of Christmas {} Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        5 => println!("On the fifth day of Christmas {} Five gold rings Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        6 => println!("On the sixth day of Christmas {} Six geese a laying Five gold rings Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        7 => println!("On the seventh day of Christmas {} Seven swans a swimming Six geese a laying Five gold rings Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        8 => println!("On the eighth day of Christmas {} Eight maids a milking Seven swans a swimming Six geese a laying Five gold rings Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        9 => println!("On the ninth day of Christmas {} Nine ladies dancing Eight maids a milking Seven swans a swimming Six geese a laying Five gold rings, badam-pam-pam Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        10 => println!("On the tenth day of Christmas {} Ten lords a leaping Nine ladies dancing Eight maids a milking Seven swans a swimming Six geese a laying Five gold rings, badam-pam-pam Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        11 => println!("On the eleventh day of Christmas  {} Eleven pipers piping Ten lords a leaping Nine ladies dancing Eight maids a milking Seven swans a swimming Six geese a laying Five gold rings, badam-pam-pam Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
-        12 => println!("On the twelfth day of Christmas {} Twelve drummers drumming Eleven pipers piping Ten lords a leaping Nine ladies dancing Eight maids a milking Seven swans a swimming Six geese a laying Five gold rings, badam-pam-pam Four calling birds Three French hens Two turtle doves And a partridge in a pear tree", WHO),
+        3 => println!("On the third day of Christmas {} {} {} {}" , WHO, gifts[2], gifts[1], gifts[0]),
+        4 => println!("On the fourth day of Christmas {} {} {} {} {}" , WHO, gifts[3], gifts[2], gifts[1], gifts[0]),
+        5 => println!("On the fifth day of Christmas {} {} {} {} {} {}" , WHO, gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        6 => println!("On the sixth day of Christmas {} {} {} {} {} {} {}" , WHO, gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        7 => println!("On the seventh day of Christmas {} {} {} {} {} {} {} {}" , WHO, gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        8 => println!("On the eighth day of Christmas {} {} {} {} {} {} {} {} {}" , WHO, gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        9 => println!("On the ninth day of Christmas {} {} {} {} {} {} {} {} {} {}" , WHO, gifts[8],gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        10 => println!("On the tenth day of Christmas {} {} {} {} {} {} {} {} {} {} {}" , WHO, gifts[9], gifts[8],gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        11 => println!("On the eleventh day of Christmas  {} {} {} {} {} {} {} {} {} {} {} {}" , WHO, gifts[10], gifts[9], gifts[8],gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
+        12 => println!("On the twelfth day of Christmas {} {} {} {} {} {} {} {} {} {} {} {} {}" , WHO, gifts[11], gifts[10], gifts[9], gifts[8],gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]),
         _ => return
     }
 }
